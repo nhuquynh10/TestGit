d30a3894134d4abf6016b37280ce1c31f885ae39
nhuquynh <toannguyen.programmer@gmail.com>
2023-09-25 13:38:34 +0700

    comit

--- WinFormsApp1/WinFormsApp1/Form1.cs
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -1,10 +1,17 @@
-namespace WinFormsApp1
+﻿namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+            /*
+             * đâu biết lần đi một lỡ làng
+             * dưới trời đau khổ chết yêu đương
+             * người xa xăm quá tôi buồn quá
+             * trong một ngày vui pháo nhuộm đường
+             */
         }
     }
 }
